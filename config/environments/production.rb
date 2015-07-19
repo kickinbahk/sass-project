e8e936e9da8760d185d67a71a64088dc1f844fa3
kickinbahk <kickinbahk@gmail.com>
2015-07-18 17:02:28 -0700

    edited config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
     :authentication => plain,
     :user_name => ENV["SENDGRID_USERNAME"],
     :password => ENV["SENDGRID_PASSWORD"],
-    :domain => 'heroku.com'
+    :domain => 'heroku.com',
     :enable_startstls_auto => true
   }
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
