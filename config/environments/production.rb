9450266aaf073a6cf50bd48cc4e8c878d25bc492
kickinbahk <kickinbahk@gmail.com>
2015-07-18 17:01:32 -0700

    edited config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,18 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.sendgrid.net",
+    :port => '587',
+    :authentication => plain,
+    :user_name => ENV["SENDGRID_USERNAME"],
+    :password => ENV["SENDGRID_PASSWORD"],
+    :domain => 'heroku.com'
+    :enable_startstls_auto => true
+  }
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
