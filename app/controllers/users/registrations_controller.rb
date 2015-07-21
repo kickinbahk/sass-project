3ade184ff911ed4a3c3cdc03a8bd603835cca343
kickinbahk <kickinbahk@gmail.com>
2015-07-21 09:39:59 -0700

    Pushing with errors

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,9 +3,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def create
     super do |resource|
       if params[:plan]
-        resources.plan_id =params[:plan]
-        if resource.plan_id ==2
-          resourcs.save_with_payment
+        resource.plan_id = params[:plan]
+        if resource.plan_id == 2
+          resource.save_with_payment
         else
           resource.save
         end
